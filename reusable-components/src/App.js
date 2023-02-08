8552aecd28cabf8b9c269d317ddcd80fa838defd
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-08 10:42:53 +0530

    Handle images in React

--- reusable-components/src/App.js
+++ reusable-components/src/App.js
@@ -1,12 +1,24 @@
+/* eslint-disable no-unused-vars */
 import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
-      <ProfileCard title='Alexa' twitterHandle='@alexa99' />
-      <ProfileCard title='Cortana' twitterHandle='@cortana32' />
-      <ProfileCard title='Siri' twitterHandle='@siri01' />
+      <ProfileCard
+        title='Alexa'
+        twitterHandle='@alexa99'
+        imageURL={AlexaImage}
+      />
+      <ProfileCard
+        title='Cortana'
+        twitterHandle='@cortana32'
+        imageURL={CortanaImage}
+      />
+      <ProfileCard title='Siri' twitterHandle='@siri01' imageURL={SiriImage} />
     </div>
   );
 }
--- reusable-components/src/ProfileCard.js
+++ reusable-components/src/ProfileCard.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-unused-vars */
-function ProfileCard({ title, twitterHandle }) {
-  // const { title, twitterHandle } = props;
+function ProfileCard({ title, twitterHandle, imageURL }) {
+  const altText = `${title} logo`;
 
   return (
     <div>
+      <img src={imageURL} alt={altText} />
       <div>Title is {title}</div>
       <div>Handle is {twitterHandle}</div>
     </div>
