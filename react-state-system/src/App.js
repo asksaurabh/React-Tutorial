8fe28d12090930e1754598db5cf5ac23a4885937
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-08 17:46:43 +0530

    Using the useState function

--- react-state-system/src/App.js
+++ react-state-system/src/App.js
@@ -1,14 +1,18 @@
 /* eslint-disable no-unused-vars */
 import AnimalShow from './AnimalShow';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   const handleClick = () => {
-    console.log('Button was clicked');
+    setCount(count + 1);
   };
 
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
+      <div>Animals to show : {count}</div>
     </div>
   );
 }
