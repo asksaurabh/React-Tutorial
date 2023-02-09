97f597b31beb80a66bc0eabf94648b31bdc11370
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-09 13:19:22 +0530

    Add axios library and make request to get images based on query search params

--- using-api-with-react/src/index.js
+++ using-api-with-react/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import searchImages from './unsplash-api';
+
+searchImages();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
