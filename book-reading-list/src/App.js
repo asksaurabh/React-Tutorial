0affd6bac5fbc11101f113dbda803f29eaafc045
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-10 11:47:35 +0530

    return the book title from child to parent component

--- book-reading-list/src/App.js
+++ book-reading-list/src/App.js
@@ -1,9 +1,19 @@
+/* eslint-disable no-unused-vars */
 import { useState } from 'react';
+import BookCreate from './components/BookCreate';
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  return <div>App</div>;
+  const createBook = (newBookTitle) => {
+    console.log('Need to add book with title of ', newBookTitle);
+  };
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
--- book-reading-list/src/components/BookCreate.js
+++ book-reading-list/src/components/BookCreate.js
@@ -1,5 +1,28 @@
-function BookCreate() {
-  return <div>BookCreate</div>;
+import { useState } from 'react';
+
+function BookCreate({ onCreate }) {
+  const [bookTitle, setBookTitle] = useState('');
+
+  const onBookSubmit = (event) => {
+    event.preventDefault();
+    onCreate(bookTitle);
+    setBookTitle('');
+  };
+
+  const onBookTitleChange = (event) => {
+    setBookTitle(event.target.value);
+  };
+
+  return (
+    <div>
+      <h4>Add a Book</h4>
+      <form onSubmit={onBookSubmit}>
+        <label>Title</label>
+        <input value={bookTitle} onChange={onBookTitleChange} />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default BookCreate;
