480e4414a424404e5855b0c481be754b0f3e256b
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-11 21:08:45 +0530

    Create context and share a number among components

--- usecontext-book-reading-app/src/index.js
+++ usecontext-book-reading-app/src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import BooksContext from './context/books';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(
+  <BooksContext.Provider value={5}>
+    <App />
+  </BooksContext.Provider>
+);
