a6274115ab54b0a131dd0d0b36f61ac6d5874ef3
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-14 21:24:00 +0530

    Handle dropdown open and close event and state

--- custom-naivgation-routing-systems/src/components/Dropdown.js
+++ custom-naivgation-routing-systems/src/components/Dropdown.js
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+
 function Dropdown({ options }) {
-  return <div>Dropdown</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleDropdownClick = () => {
+    setIsOpen((currentIsOpen) => {
+      return !currentIsOpen;
+    });
+  };
+
+  const renderedOptions = options.map((option) => {
+    return <div key={option.value}>{option.label}</div>;
+  });
+
+  const dropdownOptions = isOpen && <div>{renderedOptions}</div>;
+
+  return (
+    <div>
+      <div onClick={handleDropdownClick}>Select...</div>
+      {dropdownOptions}
+    </div>
+  );
 }
 
 export default Dropdown;
