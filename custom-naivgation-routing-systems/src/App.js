f612c6d5024c1aea9f25f739fb37aa9f1d3038d0
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-14 16:53:21 +0530

    Show content when label expanded

--- custom-naivgation-routing-systems/src/App.js
+++ custom-naivgation-routing-systems/src/App.js
@@ -5,17 +5,20 @@ function App() {
     {
       id: 'l2kj5sas',
       label: 'Can I use React in a project?',
-      content: 'You can use React on any project you want.',
+      content:
+        'You can use React on any project you want. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, possimus? Voluptatem itaque, possimus a repudiandae doloremque quia facilis veniam consequatur mollitia quidem.',
     },
     {
       id: 'l2kj5hsj',
       label: 'Can I use JavaScript in a project?',
-      content: 'You can use JavaScript on any project you want.',
+      content:
+        'You can use JavaScript on any project you want. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, possimus? Voluptatem itaque, possimus a repudiandae doloremque quia facilis veniam consequatur mollitia quidem.',
     },
     {
       id: 'l2kj5hsk',
       label: 'Can I use Python in a project?',
-      content: 'You can use Python on any project you want.',
+      content:
+        'You can use Python on any project you want. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, possimus? Voluptatem itaque, possimus a repudiandae doloremque quia facilis veniam consequatur mollitia quidem.',
     },
   ];
 
