6477141bf6c40731d321f07a9fff07fbdaeebb96
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-14 16:27:19 +0530

    Render list of items of Accordion

--- custom-naivgation-routing-systems/src/App.js
+++ custom-naivgation-routing-systems/src/App.js
@@ -3,14 +3,17 @@ import Accordion from './components/Accordion';
 function App() {
   const items = [
     {
+      id: 'l2kj5sas',
       label: 'Can I use React in a project?',
       content: 'You can use React on any project you want.',
     },
     {
+      id: 'l2kj5hsj',
       label: 'Can I use JavaScript in a project?',
       content: 'You can use JavaScript on any project you want.',
     },
     {
+      id: 'l2kj5hsk',
       label: 'Can I use Python in a project?',
       content: 'You can use Python on any project you want.',
     },
