7b97f4db4d992c66c73bc3c07f91a79a215fb07f
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-16 02:08:15 +0530

    Add a Dropdown Page

--- custom-naivgation-routing-systems/src/App.js
+++ custom-naivgation-routing-systems/src/App.js
@@ -1,42 +1,5 @@
-import { useState } from 'react';
-import Dropdown from './components/Dropdown';
-
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleOptionSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    {
-      label: 'Red',
-      value: 'red',
-    },
-    {
-      label: 'Green',
-      value: 'green',
-    },
-    {
-      label: 'Blue',
-      value: 'blue',
-    },
-  ];
-
-  return (
-    <div className='flex'>
-      <Dropdown
-        options={options}
-        value={selection}
-        onChange={handleOptionSelect}
-      />
-      <Dropdown
-        options={options}
-        value={selection}
-        onChange={handleOptionSelect}
-      />
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
