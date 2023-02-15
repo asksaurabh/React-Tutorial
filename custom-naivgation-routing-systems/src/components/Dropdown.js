1365f522cdbf040cdb2ab437bff444436c4ea7e2
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-15 11:38:43 +0530

    Add styling and icons

--- custom-naivgation-routing-systems/src/components/Dropdown.js
+++ custom-naivgation-routing-systems/src/components/Dropdown.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { GoChevronDown, GoChevronUp } from 'react-icons/go';
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,18 +17,37 @@ function Dropdown({ options, value, onChange }) {
 
   const renderedOptions = options.map((option) => {
     return (
-      <div onClick={() => handleOptionClick(option)} key={option.value}>
+      <div
+        className='hover:bg-sky-100 rounded cursor-pointer p-1'
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
         {option.label}
       </div>
     );
   });
 
   const dropdownText = value ? value.label : 'Select...';
-  const dropdownOptions = isOpen && <div>{renderedOptions}</div>;
+  const dropdownIcon = isOpen ? (
+    <GoChevronUp className='text-lg' />
+  ) : (
+    <GoChevronDown className='text-lg' />
+  );
+  const dropdownOptions = isOpen && (
+    <div className='absolute top-full border rounded p-3 shadow bg-white w-full'>
+      {renderedOptions}
+    </div>
+  );
 
   return (
-    <div>
-      <div onClick={handleDropdownClick}>{dropdownText}</div>
+    <div className='w-48 relative'>
+      <div
+        className='flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full'
+        onClick={handleDropdownClick}
+      >
+        {dropdownText}
+        {dropdownIcon}
+      </div>
       {dropdownOptions}
     </div>
   );
