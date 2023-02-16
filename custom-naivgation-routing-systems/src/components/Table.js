2e339cd3ca8ac1ead471596f2fe1e45a1ef7c9d5
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-16 19:57:42 +0530

    Add rows to our table

--- custom-naivgation-routing-systems/src/components/Table.js
+++ custom-naivgation-routing-systems/src/components/Table.js
@@ -1,5 +1,26 @@
 function Table({ data }) {
-  return <div>{data.length}</div>;
+  const renderedRows = data.map((fruit) => {
+    return (
+      <tr key={fruit.label}>
+        <td>{fruit.label}</td>
+        <td>{fruit.color}</td>
+        <td>{fruit.score}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Fruit</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
 
 export default Table;
