73e24468e271e54b78051818ed49d14b848329d4
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-16 19:44:01 +0530

    Pass table data as props

--- custom-naivgation-routing-systems/src/pages/TablePage.js
+++ custom-naivgation-routing-systems/src/pages/TablePage.js
@@ -1,9 +1,16 @@
 import Table from '../components/Table';
 
 function TablePage() {
+  const data = [
+    { label: 'Orange', color: 'bg-orange-500', score: 5 },
+    { label: 'Apple', color: 'bg-red-500', score: 3 },
+    { label: 'Yellow', color: 'bg-yellow-500', score: 1 },
+    { label: 'Lime', color: 'bg-green-500', score: 4 },
+  ];
+
   return (
     <div>
-      <Table />
+      <Table data={data} />
     </div>
   );
 }
