2488073a066c272f630d3cc4f46fb55c10fdb3d3
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-16 10:06:39 +0530

    Add event handler to listen for popstate event when going back:forth

--- custom-naivgation-routing-systems/src/index.js
+++ custom-naivgation-routing-systems/src/index.js
@@ -2,7 +2,7 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { NavigationProvider } from './context/Navigation';
+import { NavigationProvider } from './context/navigation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
