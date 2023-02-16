4a5f41180730563c64a0c396d5535ea55343bd64
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-16 13:58:23 +0530

    Toggle Modal Visibility

--- custom-naivgation-routing-systems/src/pages/ModalPage.js
+++ custom-naivgation-routing-systems/src/pages/ModalPage.js
@@ -1,9 +1,21 @@
 import Modal from '../components/Modal';
+import Button from '../components/Button';
+import { useState } from 'react';
 
 function ModalPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleClick = () => {
+    console.log('Open Modal');
+    setIsModalOpen(true);
+  };
+
   return (
     <div>
-      <Modal />
+      <Button onClick={handleClick} primary>
+        Open Modal
+      </Button>
+      {isModalOpen && <Modal />}
     </div>
   );
 }
