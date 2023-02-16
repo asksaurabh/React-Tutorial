957453fe1555f7f6839acf0adf2a888d279583cc
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-16 20:29:28 +0530

    Create dynamic table header using config prop

--- custom-naivgation-routing-systems/src/components/Table.js
+++ custom-naivgation-routing-systems/src/components/Table.js
@@ -1,8 +1,12 @@
-function Table({ data }) {
+function Table({ data, config }) {
+  const renderedHeader = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+
   const renderedRows = data.map((fruit) => {
     return (
-      <tr className='border-b' key={fruit.label}>
-        <td className='p-3'>{fruit.label}</td>
+      <tr className='border-b' key={fruit.name}>
+        <td className='p-3'>{fruit.name}</td>
         <td className='p-3'>
           <div className={`p-3 m-2 ${fruit.color}`}></div>
         </td>
@@ -14,11 +18,7 @@ function Table({ data }) {
   return (
     <table className='table-auto border-spacing-2'>
       <thead>
-        <tr className='border-b-2'>
-          <th>Fruit</th>
-          <th>Color</th>
-          <th>Score</th>
-        </tr>
+        <tr className='border-b-2'>{renderedHeader}</tr>
       </thead>
       <tbody>{renderedRows}</tbody>
     </table>
--- custom-naivgation-routing-systems/src/pages/TablePage.js
+++ custom-naivgation-routing-systems/src/pages/TablePage.js
@@ -2,15 +2,27 @@ import Table from '../components/Table';
 
 function TablePage() {
   const data = [
-    { label: 'Orange', color: 'bg-orange-500', score: 5 },
-    { label: 'Apple', color: 'bg-red-500', score: 3 },
-    { label: 'Yellow', color: 'bg-yellow-500', score: 1 },
-    { label: 'Lime', color: 'bg-green-500', score: 4 },
+    { name: 'Orange', color: 'bg-orange-500', score: 5 },
+    { name: 'Apple', color: 'bg-red-500', score: 3 },
+    { name: 'Yellow', color: 'bg-yellow-500', score: 1 },
+    { name: 'Lime', color: 'bg-green-500', score: 4 },
+  ];
+
+  const config = [
+    {
+      label: 'Fruit',
+    },
+    {
+      label: 'Color',
+    },
+    {
+      label: 'Score',
+    },
   ];
 
   return (
     <div>
-      <Table data={data} />
+      <Table data={data} config={config} />
     </div>
   );
 }
