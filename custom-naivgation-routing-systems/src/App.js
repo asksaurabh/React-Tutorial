f3d938e5dc7702d79d299e002ab21556ab853383
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-16 12:57:52 +0530

    Add the SideBar component to show a list of links

--- custom-naivgation-routing-systems/src/App.js
+++ custom-naivgation-routing-systems/src/App.js
@@ -1,20 +1,22 @@
-import Link from './components/Link';
+import SideBar from './components/SideBar';
 import Route from './components/Route';
 import AccordionPage from './pages/AccordianPage';
 import DropdownPage from './pages/DropdownPage';
+import ButtonPage from './pages/ButtonPage';
 
 function App() {
   return (
-    <div>
-      <Link to='/accordion'>Go to Accordion</Link>
-      <Link to='/dropdown'>Go to Dropdown</Link>
-
-      <div>
+    <div className='container mx-auto grid grid-cols-6 gap-4 mt-4'>
+      <SideBar />
+      <div className='col-span-5'>
+        <Route path='/'>
+          <DropdownPage />
+        </Route>
         <Route path='/accordion'>
           <AccordionPage />
         </Route>
-        <Route path='/dropdown'>
-          <DropdownPage />
+        <Route path='/buttons'>
+          <ButtonPage />
         </Route>
       </div>
     </div>
