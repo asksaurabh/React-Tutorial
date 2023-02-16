a8aab5dec77c62669e8462891ad8f6c61f9c92ab
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-16 12:42:05 +0530

    Add custom hook to use Navigation Context

--- custom-naivgation-routing-systems/src/components/Link.js
+++ custom-naivgation-routing-systems/src/components/Link.js
@@ -1,9 +1,8 @@
-import { useContext } from 'react';
-import NavigationContext from '../context/navigation';
+import useNavigationContext from '../hooks/use-navigation';
 import classNames from 'classnames';
 
 function Link({ to, children }) {
-  const { navigate } = useContext(NavigationContext);
+  const { navigate } = useNavigationContext();
 
   const classes = classNames('text-blue-500');
 
--- custom-naivgation-routing-systems/src/components/Route.js
+++ custom-naivgation-routing-systems/src/components/Route.js
@@ -1,8 +1,7 @@
-import { useContext } from 'react';
-import NavigationContext from '../context/navigation';
+import useNavigationContext from '../hooks/use-navigation';
 
 function Route({ path, children }) {
-  const { currentPath } = useContext(NavigationContext);
+  const { currentPath } = useNavigationContext();
 
   if (path === currentPath) {
     return children;
