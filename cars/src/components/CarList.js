830d320d973de8ba5085ea7d2f1a592ce4754cbf
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-20 15:42:53 +0530

    Handle cars list

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -1,15 +1,38 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeCar } from '../store';
 
 function CarList() {
+  const dispatch = useDispatch();
   const cars = useSelector((state) => {
     return state.cars.carsList;
   });
 
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car.id));
+  };
+
   const renderedCars = cars.map((car) => {
-    return <li key={car.id}>{car.carName}</li>;
+    return (
+      <div key={car.id} className='panel'>
+        <p>
+          {car.carName} - ${car.carCost}
+        </p>
+        <button
+          className='button is-danger'
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    );
   });
 
-  return <div>{renderedCars}</div>;
+  return (
+    <div className='car-list'>
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
