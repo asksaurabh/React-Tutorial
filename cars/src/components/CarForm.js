4ed5b67dfffdb4acf892c6b4867fbba7c6bc0063
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-20 14:13:53 +0530

    Access and update the carName input state

--- cars/src/components/CarForm.js
+++ cars/src/components/CarForm.js
@@ -1,5 +1,33 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { changeName } from '../store';
+
 function CarForm() {
-  return <div>CarForm</div>;
+  const dispatch = useDispatch();
+  const carName = useSelector((state) => {
+    return state.form.carName;
+  });
+
+  const handleCarNameChange = (event) => {
+    dispatch(changeName(event.target.value));
+  };
+
+  return (
+    <div className='car-form panel'>
+      <h4 className='subtitle is-3'>Add Car</h4>
+      <form>
+        <div className='field-group'>
+          <div className='field'>
+            <label className='label'>Name</label>
+            <input
+              className='input is-expanded'
+              value={carName}
+              onChange={handleCarNameChange}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CarForm;
