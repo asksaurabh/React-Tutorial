7da65cc1891625f4331c2a3b7997d1fad124c31c
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-20 14:32:00 +0530

    Access and update the carCost input and handle form submission

--- cars/src/components/CarForm.js
+++ cars/src/components/CarForm.js
@@ -1,20 +1,35 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { changeName } from '../store';
+import { changeName, changeCost, addCar } from '../store';
 
 function CarForm() {
   const dispatch = useDispatch();
-  const carName = useSelector((state) => {
-    return state.form.carName;
+  const { carName, carCost } = useSelector((state) => {
+    return {
+      carName: state.form.carName,
+      carCost: state.form.carCost,
+    };
   });
 
   const handleCarNameChange = (event) => {
     dispatch(changeName(event.target.value));
   };
 
+  const handleCostChange = (event) => {
+    const value = parseInt(event.target.value) || 0;
+    dispatch(changeCost(value));
+  };
+
+  const handleCarSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addCar({ carName, carCost }));
+    dispatch(changeName(''));
+    dispatch(changeCost(0));
+  };
+
   return (
     <div className='car-form panel'>
       <h4 className='subtitle is-3'>Add Car</h4>
-      <form>
+      <form onSubmit={handleCarSubmit}>
         <div className='field-group'>
           <div className='field'>
             <label className='label'>Name</label>
@@ -24,6 +39,24 @@ function CarForm() {
               onChange={handleCarNameChange}
             />
           </div>
+        </div>
+
+        <div className='field-group'>
+          <div className='field'>
+            <label className='label'>Cost</label>
+            <input
+              className='input is-expanded'
+              value={carCost || ''}
+              onChange={handleCostChange}
+              type='number'
+            />
+          </div>
+        </div>
+
+        <div className='field'>
+          <button button is-link>
+            Submit
+          </button>
         </div>
       </form>
     </div>
