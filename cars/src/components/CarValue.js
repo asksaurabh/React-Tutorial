202856f561052a5d4b9a00efb98d59f7ae8d9598
Saurabh Kumar <asksaurabh101@gmail.com>
2023-02-20 16:40:14 +0530

    Add car cost

--- cars/src/components/CarValue.js
+++ cars/src/components/CarValue.js
@@ -1,5 +1,15 @@
+import { useSelector } from 'react-redux';
+
 function CarValue() {
-  return <div>CarValue</div>;
+  const totalCost = useSelector(({ cars: { searchTerm, carsList } }) => {
+    return carsList
+      .filter((car) => {
+        return car.carName.toLowerCase().includes(searchTerm.toLowerCase());
+      })
+      .reduce((sum, car) => sum + car.carCost, 0);
+  });
+
+  return <div className='car-value'>Total Cost: ${totalCost}</div>;
 }
 
 export default CarValue;
